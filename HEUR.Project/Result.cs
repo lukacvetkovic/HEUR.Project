e4c1a04dd2a1ffd13acf5f070a9f6282a772ce78
Luka Cvetkovic <cvetko911@hotmail.com>
2016-12-17 14:31:45 +0100

    Result

---
+++ HEUR.Project/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEUR.Project
+{
+    public class Result
+    {
+        public int[,] x;
+        public List<Route> routes;
+    }
+}
---
+++ HEUR.Project/Route.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEUR.Project
+{
+    public class Route
+    {
+        public int componentOne { get; set; }
+        public int componentTwo { get; set; }
+        public List<int> comunicationNodes { get; set; }
+
+    }
+}

374c205250dfb0ecbc387397fa14333badd0f228
Luka Cvetkovic <cvetko911@hotmail.com>
2016-12-17 13:18:43 +0100

    InputParameters

---
+++ HEUR.Project/NodeConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEUR.Project
+{
+    public class NodeConnection
+    {
+        public int firstNode { get; set; }
+        public int secondNode { get; set; }
+        public int capacity { get; set; }
+        public double energyConsumption { get; set; }
+        public double latency { get; set; }
+
+        public NodeConnection(int firstNode, int secondNode, int capacity, double energyConsumption, double latency)
+        {
+            this.firstNode = firstNode;
+            this.secondNode = secondNode;
+            this.capacity = capacity;
+            this.energyConsumption = energyConsumption;
+            this.latency = latency;
+        }
+    }
+}
---
+++ HEUR.Project/RequiredBandwidth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEUR.Project
+{
+    public class RequiredBandwidth
+    {
+        public int componentOne { get; set; }
+        public int componentTwo { get; set; }
+        public int bandwith { get; set; }
+
+        public RequiredBandwidth(int componentOne, int componentTwo, int bandwith)
+        {
+            this.componentOne = componentOne;
+            this.componentTwo = componentTwo;
+            this.bandwith = bandwith;
+        }
+    }
+}

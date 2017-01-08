d80d74b23cafcfa9378ddf59353fb903232eb482
Luka Cvetkovic <cvetko911@hotmail.com>
2017-01-08 14:57:04 +0100

    ispis

--- HEUR.Project/Program.cs
+++ HEUR.Project/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,8 +18,10 @@ namespace HEUR.Project
                 Result result = ResultGenerator.NaiveResult();
                 if (result.CannotFind==0)
                 {
-                    Console.WriteLine("NADENO RJESENJE WOHOOO");
+                    Console.WriteLine("NADENO RJESENJE "+ resultList.Count + 1);
                     resultList.Add(result);
+
+                    PrintResult(result, "Result_"+resultList.Count+1+".txt");
                 }
 
                 //while (!result.IsValid())
@@ -26,6 +30,49 @@ namespace HEUR.Project
                 //}
 
             } while (resultList.Count<=10);
+        }
+
+        private static void PrintResult(Result result, string filename)
+        {
+            using (StreamWriter writetext = new StreamWriter(filename))
+            {
+                writetext.WriteLine("x=[");
+                
+                for (int i = 0 ; i<result.x.GetLength(0);i++)
+                {
+                    String line = "[";
+                    for (int j = 0; j < result.x.GetLength(1); j++)
+                    {
+                        line += result.x[i, j] + ",";
+                    }
+
+                    line = line.Remove(line.Length - 1, 1) + "]";
+
+                    writetext.WriteLine(line);
+                }
+
+                writetext.WriteLine("];");
+
+                writetext.WriteLine();
+
+                writetext.WriteLine("routes={");
+
+                String routeLine = "";
+                foreach (var route in result.routes)
+                {
+                    routeLine="<"+route.componentOne+","+route.componentTwo+",["+string.Join(",", route.comunicationNodes) +"]>,";
+
+                    if (result.routes.Last() == route)
+                    {
+                        routeLine = routeLine.Remove(routeLine.Length - 1, 1);
+                    }
+
+                    writetext.WriteLine(routeLine);
+                }
+
+
+                writetext.WriteLine("};");
+            }
         }
     }
 }
